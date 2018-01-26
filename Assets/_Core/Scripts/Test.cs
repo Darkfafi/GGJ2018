202208439cbd6e79a8b981f9e3d37254e7d8703b
Ramses di Perna <15826@ma-web.nl>
2018-01-26 23:19:35 +0100

    Added DoTween

---
+++ Assets/_Core/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Test : MonoBehaviour {
+
+	
+    protected void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.DOMoveX(1, 1.2f).SetEase(Ease.OutBack).OnComplete(()=> { GetComponent<SpriteRenderer>().DOColor(Color.red, 1).SetEase(Ease.OutBounce); });
+        }
+    }
+
+}
