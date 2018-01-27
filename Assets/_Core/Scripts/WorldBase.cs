95576bd17aa49b4bb25210a219800147070fe95e
Nick Aarts <>
2018-01-27 19:53:37 +0100

    Fixed stuff

--- Assets/_Core/Scripts/WorldBase.cs
+++ Assets/_Core/Scripts/WorldBase.cs
@@ -8,13 +8,14 @@ public class WorldBase : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Vector3 oldScale = transform.localScale;
-        float percentage = 0.05f;
+        float percentage = 0.08f;
+        float time = 7f;
 
-        transform.localScale = new Vector3(oldScale.x * (1 - percentage), oldScale.y * (1 + percentage), oldScale.z * (1 - percentage));
+        transform.localScale = new Vector3(oldScale.x * (1 - percentage), oldScale.y * (1 - percentage), oldScale.z * (1 - percentage));
 
-        transform.DOScaleX(oldScale.x * (1 + percentage), 5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
-        transform.DOScaleY(oldScale.y * (1 - percentage), 5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
-        transform.DOScaleZ(oldScale.z * (1 + percentage), 5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        transform.DOScaleX(oldScale.x * (1 + percentage), time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        transform.DOScaleY(oldScale.y * (1 + percentage), time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        transform.DOScaleZ(oldScale.z * (1 + percentage), time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
         /*
         tweenSequenceX.Append();
 
