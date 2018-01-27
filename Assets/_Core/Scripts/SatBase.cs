e2d87eb116840ce9deacc31998395ae3568d85db
Unknown <simon.alkemade@gmail.com>
2018-01-27 02:02:33 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/_Core/Scripts/SatBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatBase : MonoBehaviour {
+
+	public Vector3 orbitRotation;
+
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+
+		transform.Rotate(orbitRotation);
+	}
+}
