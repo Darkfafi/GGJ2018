76e4098273b27133f13e6e87fe7b1aa0b66ab628
DESKTOP-IOTBAVS\David Elout <dedavid.elout@gmail.com>
2018-01-27 19:09:47 +0100

    CReater part of audiomanagerscript

--- Assets/_Core/Scripts/Utils/AudioManager.cs
+++ Assets/_Core/Scripts/Utils/AudioManager.cs
@@ -2,15 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public enum SoundType
+{
+    SndSignalUp,
+    SndSignalDown,
+    SndShootRocket,
+    SndImpactRocket,
+    SndSattiliteIdle,
+    SndSattiliteCrash,
+    SndSattiliteFalling,
+    SndShockwave,
+    SndGetPoints,
+    SndLosePoints,
+    SndMenuClick,
+    SndMenuIdle,
+    SndGameIdle,
+    SndEnemyIdle,
+    SndEnemyShoot,
+    SndEnemyCrash,
+    SndTowerDeactive,
+    SndTowerMalfunction,
+    SndTowerReActive,
+    SndTowerReFuntion,
+    SndArnoldGetDown
+}
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    //private Text _scoreText = null;
+
+    public static AudioManager _instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<AudioManager>();
+
+                if (_instance == null)
+                {
+                    GameObject container = new GameObject("AudioManager");
+                    _instance = container.AddComponent<AudioManager>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    public void PlaySound(SoundType type)
+    {
+        switch (type)
+        {
+            case SoundType.Damage:
+
+                break;
+            default:
+                break;
+        }
+
+
+
+    }
 }
