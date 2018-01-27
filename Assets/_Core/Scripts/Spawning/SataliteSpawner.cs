520321575f27dabbbad58bb9eceb8cc49e6d6381
Ramses di Perna <15826@ma-web.nl>
2018-01-27 14:27:10 +0100

    Launching and satellite spawning made

---
+++ Assets/_Core/Scripts/Spawning/SataliteSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SataliteSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private SatelliteBase satelitePrefab;
+
+    [SerializeField]
+    private SpaceLaunch launcher;
+
+    [SerializeField]
+    private Transform Center;
+
+    protected void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnSatelite();
+        }
+    }
+
+    private void SpawnSatelite()
+    {
+        SatelliteBase sb = Instantiate(satelitePrefab, transform.position, transform.rotation);
+        sb.Visual.transform.position = transform.position;
+
+        Vector3 randomAngleVec = new Vector3(UnityEngine.Random.Range(-5, 5), 5, UnityEngine.Random.Range(-5, 5));
+
+        launcher.SendToPointInSpace(sb, randomAngleVec, Modes.MEO, 3);
+    }
+}
