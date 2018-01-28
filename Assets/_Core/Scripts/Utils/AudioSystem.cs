e30edc677081bcb39329b494f75ee9a6d9555ce9
Ramses di Perna <15826@ma-web.nl>
2018-01-28 09:34:49 +0100

    Created AudioSystem

---
+++ Assets/_Core/Scripts/Utils/AudioSystem.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class AudioSystem : MonoBehaviour
+{
+    public static AudioSystem Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new GameObject("<AudioSystem>").AddComponent<AudioSystem>();
+                _instance.gameObject.isStatic = true;
+                DontDestroyOnLoad(_instance);
+                _instance.ConStruct();
+            }
+
+            return _instance;
+        }
+    }
+    private static AudioSystem _instance;
+
+
+    public const int DEFAULT_STATION = 0;
+    public const int EFFECTS_STATION = 1;
+    public const int MUSIC_STATION = 2;
+
+    private const float startDefaultVolume = 1f;
+    private const float startMusicVolume = 1f;
+    private const float startEffectsVolume = 1f;
+
+    private const float startDefaultPitch = 1f;
+    private const float startMusicPitch = 1f;
+    private const float startEffectsPitch = 1;
+
+    public AudioLibrary Library { get; private set; }
+    private Dictionary<int, AudioSource> audioStations = new Dictionary<int, AudioSource>();
+    private Dictionary<int, StationSettings> audioStationsSettings = new Dictionary<int, StationSettings>(); //1.15
+
+    public void PlayAudio(string name, int station = 0, float volumeScale = 1)
+    {
+        PlayAudio(Library.GetAudioClip(name), station, volumeScale);
+    }
+
+    public void PlaySoloAudio(string name, int station = 0, float volumeScale = 1)
+    {
+        PlaySoloAudio(Library.GetAudioClip(name), station, volumeScale);
+    }
+
+    public void PlaySoloAudio(AudioClip clip, int station = 0, float volumeScale = 1)
+    {
+        if (audioStations.ContainsKey(station))
+        {
+            if (audioStations[station] == null)
+            {
+                audioStations.Remove(station);
+            }
+        }
+        AudioSource currentStation = GetAudioStation(station);
+        currentStation.clip = clip;
+        currentStation.Play();
+    }
+
+    public void SoloAudioLoopToggle(int station, bool loop)
+    {
+        GetStationSettings(station).Loop = loop;
+        SetSettingsForAudioStation(station);
+    }
+
+    public void StopAudio()
+    {
+        foreach (KeyValuePair<int, AudioSource> pair in audioStations)
+        {
+            StopAudio(pair.Key);
+        }
+    }
+
+    public void StopAudio(int station)
+    {
+        if (audioStations.ContainsKey(station))
+        {
+            audioStations[station].Stop();
+        }
+    }
+
+    public void PlayAudio(AudioClip clip, int station = 0, float volumeScale = 1)
+    {
+        if (audioStations.ContainsKey(station))
+        {
+            if (audioStations[station] == null)
+            {
+                audioStations.Remove(station);
+            }
+        }
+        GetAudioStation(station).PlayOneShot(clip, volumeScale);
+    }
+
+    public AudioSource GetAudioStation(int station)
+    {
+        if (!audioStations.ContainsKey(station))
+        {
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+            audioStations.Add(station, source);
+            SetSettingsForAudioStation(station);
+        }
+        return audioStations[station];
+    }
+
+    private StationSettings GetStationSettings(int station)
+    {
+        if (!audioStationsSettings.ContainsKey(station))
+        {
+            audioStationsSettings.Add(station, new StationSettings());
+        }
+        return audioStationsSettings[station];
+    }
+
+    private void SetSettingsForAudioStation(int station)
+    {
+        StationSettings settings = GetStationSettings(station);
+        AudioSource aStation = GetAudioStation(station);
+        aStation.pitch = settings.Pitch;
+        aStation.volume = settings.Volume;
+        aStation.loop = settings.Loop;
+    }
+
+    public float GetAudioStationVolume(int station)
+    {
+        return GetStationSettings(station).Volume;
+    }
+
+    public void SetAudioStationVolume(int station, float volume)
+    {
+        GetStationSettings(station).Volume = volume;
+        SetSettingsForAudioStation(station);
+    }
+
+    public float GetAudioStationPitch(int station)
+    {
+        return GetStationSettings(station).Pitch;
+    }
+
+    public void SetAudioStationPitch(int station, float pitch)
+    {
+        GetStationSettings(station).Pitch = pitch;
+        SetSettingsForAudioStation(station);
+    }
+
+    public void ConStruct()
+    {
+        Library = Resources.Load<AudioLibrary>("Libraries/AudioLibrary");
+
+        SetAudioStationVolume(DEFAULT_STATION, startDefaultVolume);
+        SetAudioStationVolume(EFFECTS_STATION, startEffectsVolume);
+        SetAudioStationVolume(MUSIC_STATION, startMusicVolume);
+        SetAudioStationPitch(DEFAULT_STATION, startDefaultPitch);
+        SetAudioStationPitch(EFFECTS_STATION, startEffectsPitch);
+        SetAudioStationPitch(MUSIC_STATION, startMusicPitch);
+        SoloAudioLoopToggle(MUSIC_STATION, true);
+    }
+
+    private class StationSettings
+    {
+        public float Pitch = 1;
+        public float Volume = 1;
+        public bool Loop = false;
+    }
+}
